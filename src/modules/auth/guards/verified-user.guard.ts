b1a04bebe025c79bfaef6ab6bb83ad0ef6e1237b
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-04 15:50:35 +0200

    feat(stamp): add VerifiedUserGuard with @Public() bypass support

---
+++ src/modules/auth/guards/verified-user.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+
+@Injectable()
+export class VerifiedUserGuard extends AuthGuard('user-jwt') {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) return true;
+    return super.canActivate(context);
+  }
+}
