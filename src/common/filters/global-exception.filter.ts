fe2d3bec7bb2acbcf2fa18750ca38dc841d0570f
Daniel Kajuga <kajugadaniels@gmail.com>
2026-06-23 12:40:13 +0200

    refactor(prisma): import prisma errors from shared database package

--- src/common/filters/global-exception.filter.ts
+++ src/common/filters/global-exception.filter.ts
@@ -6,7 +6,7 @@ import {
   HttpStatus,
   Logger,
 } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+import { Prisma } from '@gracon/database';
 import { Response } from 'express';
 
 @Catch()
